323e4eddd62a68970a918ec244234971eebcfa3f
MannyD16 <139398471+MannyD16@users.noreply.github.com>
2025-03-02 02:08:29 -0500

    Initial commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Manuel Diaz on 3/2/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
